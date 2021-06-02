37be1792ea7f60c8d48c7176996c31712ef4113b
mramasubbu@mcci.com <muthu.ramasubbu@mcci.com>
2021-06-02 17:49:46 +0530

    modified sketch

--- Blinking_LED_Fsm.ino
+++ Blinking_LED_Fsm.ino
@@ -1,6 +1,6 @@
 /*
 
-Module:  catena_fsm.ino
+Module: Blinking_LED_fsm.ino
 
 Function:
 	Example of how to build a simple FSM.
@@ -10,38 +10,29 @@ Copyright notice and License:
 	https:://github.com/mcci-catena/catena-arduino-platform/
 
 Author:
-	Terry Moore, MCCI Corporation  April 2018
+	Rama subbu, MCCI Corporation  June 2021
 
 */
 
 #include <Catena.h>
-#include <Catena_CommandStream.h>
 #include <Catena_FSM.h>
 #include <Catena_Led.h>
-
-/*
-|| This header file fixes things related to symbolic types, to allow us to
-|| build with Visual Micro. It's not needed if using a .cpp file, or if
-|| the command processor table and functions are all external. It's also not
-|| needed unless you're using Visual Micro.
-*/
-#include <Catena_CommandStream_vmicro_fixup.h>
 
 // save lots of typing by using the McciCatena namespace.
 using namespace McciCatena;
 
 /****************************************************************************\
 |
-|   The Turnstile class and FSM. Refer to the overall README.md for a
+|   The led class and FSM. Refer to the overall README.md for a
 |   description of the FSM.
 |
 \****************************************************************************/
 
-class Turnstile
+class led
 	{
 public:
 	// constructor
-	Turnstile(Catena &myCatena, StatusLed &lockLed)
+	led(Catena &myCatena, StatusLed &lockLed)
 		: m_Catena(myCatena),
 		 m_Led(lockLed)
 		{}
@@ -50,14 +41,17 @@ public:
 	enum class State
 		{
 		stNoChange = 0, // this name must be present: indicates "no change of state"
-		stInitial,	// this name must be presnt: it's the starting state.
+		stInitial,		// this name must be presnt: it's the starting state.
+		stLedOn,
+		stLedOff,
+		stFinal, 		// this name must be present, it's the terminal state.
 		};
 
 	// the begin method initializes the fsm
 	void begin()
 		{
 		if (! this->m_fRunning)
-			this->m_fsm.init(*this, &Turnstile::fsmDispatch);
+			this->m_fsm.init(*this, &led::fsmDispatch);
 		else
 			this->m_Catena.SafePrintf("already running!\n");
 		}
@@ -75,7 +69,7 @@ public:
 
 private:
 	// the FSM instance
-	cFSM<Turnstile, State> m_fsm;
+	cFSM<led, State> m_fsm;
 
 	// verify that FSM is running, and print a message if not.
 	bool checkRunning() const
@@ -89,81 +83,65 @@ private:
 			}
 		}
 
-	// "set the lock" - which means display a message
-	void setLock(bool fState)
-		{
-		if (fState)
-			{
-			m_Led.Set(LedPattern::FiftyFiftySlow);
-			m_Catena.SafePrintf("**LOCKED, deposit coin**\n");
-			}
-		else
-			{
-			m_Led.Set(LedPattern::FastFlash);
-			m_Catena.SafePrintf("**Unlocked, please proceed**\n");
-			}
-		}
-
 	// the FSM dispatch function called by this->m_fsm.
 	// this example is here, so we write it here, but in
 	// any more complex application you'd have this outside
 	// the Class definition, possibly compiled separately.
-	State fsmDispatch(State currentState, bool fEntry)
+	 State fsmDispatch(State currentState, bool fEntry)
 		{
-				  void blink(bool reset = false)
-		{
-		const uint32_t LED_DELAY = 1000;
-		static enum { LED_TOGGLE, WAIT_DELAY } state = 0;
-		static uint32_t timeLastTransition = 0;
-
-		if (reset)
-			{ 
-			state = LED_TOGGLE;
-			digitalWrite(D13, LOW);
-			}
 		State newState = State::stNoChange;
 
 		switch (currentState)
 			{
 
-			  case 0:   // toggle the LED
-			    digitalWrite(D13, !digitalRead(D13));
-			    timeLastTransition = millis();
-			    state = 1;
-			    break;
+		case State::stInitial:
+			if (fEntry)
+				{
+				Serial.begin(9600);
+				}
+			this->m_fRunning = true;
+			newState = State::stLedOn;
+			break;
 
-			  case 1:   // wait for the delay period
-			    if (millis() - timeLastTransition >= LED_DELAY)
-			      state = 0;
-			    break;
+		case State::stLedOn:
+			if (fEntry)		
+				{
+				pinMode(13, OUTPUT);
+				digitalWrite(13, HIGH);
+				delay(1000);	
+				}
+			break;
 
-			  default:
-			    state = LED_TOGGLE;
-			    break
+		case State::stLedOff:
+			if (fEntry)
+				{
+				digitalWrite(13, LOW);
+								delay(1000);
+				}
+
+		case State::stFinal:
+			// This is called just once; we just clear the
+			// running flag.  The core FSM is responsible for
+			// determining if we're locked or unlocked on
+			// exit. Since we always get here via the stLocked
+			// state, we will, in fact, be locked.
+			if (fEntry)
+				{
+				m_Catena.SafePrintf("led stopped!\n");
+				this->m_fRunning = false;
+				}
+			else
+				{
+				m_Catena.SafePrintf("stFinal but not fEntry shouldn't happen.\n");
+				}
+
+			// stay in this state.
+			break;
 			}
-		}
 
 		return newState;
 		}
-
-	// the Catena reference
-	Catena &m_Catena;
-	StatusLed &m_Led;
-
-	// the event flags:
-
-	// a coin has been dropped
-	bool m_evCoin : 1;
-
-	// someone has pushed on the turnstile.
-	bool m_evPush : 1;
-
-	// a shutdown has been requested
-	bool m_evShutdown : 1;
-
-	// state flag: true iff the FSM is running.
-	bool m_fRunning: 1;
-	};
+	}
 
 /****************************************************************************\
 |
@@ -177,38 +155,13 @@ Catena gCatena;
 // instantiate the LED object
 StatusLed gLed (Catena::PIN_STATUS_LED);
 
-// instantiate the turnstile
-Turnstile gTurnstile (gCatena, gLed);
+// instantiate the led
+led gled (gCatena, gLed);
 
 /*
 || The next few lines give the datastructures needed for extending
 || the command parser.
 */
-
-// forward reference to the command function
-cCommandStream::CommandFn cmdCoin;
-cCommandStream::CommandFn cmdPush;
-cCommandStream::CommandFn cmdBegin;
-cCommandStream::CommandFn cmdEnd;
-
-// the individual commmands are put in this table
-static const cCommandStream::cEntry sMyExtraCommmands[] =
-	{
-	{ "begin", cmdBegin },
-	{ "coin", cmdCoin },
-	{ "push", cmdPush },
-	{ "end", cmdEnd },
-	// other commands go here
-	};
-
-/* a top-level structure wraps the above and connects to the system table */
-/* it optionally includes a "first word" so you can for sure avoid name clashes */
-static cCommandStream::cDispatch
-sMyExtraCommands_top(
-	sMyExtraCommmands,		/* this is the pointer to the table */
-	sizeof(sMyExtraCommmands),	/* this is the size of the table */
-	nullptr				/* this is no "first word" for all the commands in this table */
-	);
 
 /****************************************************************************\
 |
@@ -228,7 +181,7 @@ void setup()
 
 	/* wait for a UART connection */
 	while (! Serial)
-		/* wait */;
+	/* wait */;
 
 	/* add our application-specific commands */
 	gCatena.addCommands(
@@ -249,7 +202,7 @@ void setup()
 
 	gLed.begin();
 	gCatena.registerObject(&gLed);
-	gTurnstile.begin();
+	gled.begin();
 	}
 
 // loop is called repeatedly.
@@ -259,74 +212,4 @@ void loop()
 	// polling framework. Everytng wlse will be driven from that.
 	gCatena.poll();
 	}
-
-/****************************************************************************\
-|
-|   The commands -- called automatically from the framework after receiving
-|   and parsing a command from the Serial console.
-|
-\****************************************************************************/
-
-/* process "coin" -- args are ignored */
-// argv[0] is "coin"
-// argv[1..argc-1] are the (ignored) arguments
-cCommandStream::CommandStatus cmdCoin(
-	cCommandStream *pThis,
-	void *pContext,
-	int argc,
-	char **argv
-	)
-	{
-	pThis->printf("%s\n", argv[0]);
-	gTurnstile.evCoin();
-
-	return cCommandStream::CommandStatus::kSuccess;
-	}
-
-/* process "push" -- args are ignored */
-// argv[0] is "push"
-// argv[1..argc-1] are the (ignored) arguments
-cCommandStream::CommandStatus cmdPush(
-	cCommandStream *pThis,
-	void *pContext,
-	int argc,
-	char **argv
-	)
-	{
-	pThis->printf("%s\n", argv[0]);
-	gTurnstile.evPush();
-
-	return cCommandStream::CommandStatus::kSuccess;
-	}
-
-/* process "begin" -- args are ignored */
-// argv[0] is "stop"
-// argv[1..argc-1] are the (ignored) arguments
-cCommandStream::CommandStatus cmdBegin(
-	cCommandStream *pThis,
-	void *pContext,
-	int argc,
-	char **argv
-	)
-	{
-	pThis->printf("%s\n", argv[0]);
-	gTurnstile.begin();
-
-	return cCommandStream::CommandStatus::kSuccess;
-	}
-
-/* process "end" -- args are ignored */
-// argv[0] is "end"
-// argv[1..argc-1] are the (ignored) arguments
-cCommandStream::CommandStatus cmdEnd(
-	cCommandStream *pThis,
-	void *pContext,
-	int argc,
-	char **argv
-	)
-	{
-	pThis->printf("%s\n", argv[0]);
-	gTurnstile.end();
-
-	return cCommandStream::CommandStatus::kSuccess;
-	}
+	
